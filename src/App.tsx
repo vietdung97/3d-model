8b506a3fd1a5b9469da0baa106d4789ce52e16f4
vietdung97 <ngovietdung0112@gmail.com>
2023-10-24 17:38:46 +0700

    Update code

--- src/App.tsx
+++ src/App.tsx
@@ -8,17 +8,20 @@ import { Gender } from "./constants/boundaries";
 import touchBodyPart from "./constants/touchBodyPart";
 import "./index.css";
 
-const Scene = ({ gender}: {gender: Gender}) => {
+const Scene = ({ gender, autoRotation }: { gender: Gender; autoRotation: boolean }) => {
   const ref = useRef<any>();
   const obj = useLoader(OBJLoader, "FinalBaseMesh.obj");
   const obj2 = useLoader(FBXLoader, "FemaleBodyModal.fbx");
   // Subscribe this component to the render-loop, rotate the mesh every frame
-  useFrame(() => (ref.current.rotation.y += 0.002));
+  useFrame(() => {
+    if(!autoRotation) return;
+    ref.current.rotation.y += 0.002
+  });
 
   // Here, we can access the camera via the useThree hook
   useThree(
     ({ camera }) => {
-      const positionY = gender === 'male' ? 18 : 120;
+      const positionY = gender === "male" ? 18 : 120;
       camera.position.y = positionY;
       camera.lookAt(0, 0, 0);
     },
@@ -26,57 +29,54 @@ const Scene = ({ gender}: {gender: Gender}) => {
       return false;
     }
   );
-  const object = gender === 'male' ? obj : obj2;
-  const position = gender === 'male' ?  [0, -10, 0] : [0, -60, 0];
+  const object = gender === "male" ? obj : obj2;
+  const position = gender === "male" ? [0, -10, 0] : [0, -60, 0];
 
   useLayoutEffect(() => {
     object.traverse((child: any) => {
-      if (child.isMesh) child.material.color.set('red')
-    })
-  }, [object])
+      if (child.isMesh) child.material.color.set("red");
+    });
+  }, [object]);
 
   return (
-
-      <primitive
-        ref={ref}
-        object={object}
-        scale={1}
-        position={position}
-        onClick={(e: any) => {
-          const part = touchBodyPart(e.point, gender);
-          if((window as any).ReactNativeWebView) {
-            (window as any).ReactNativeWebView?.postMessage(JSON.stringify({ part }));
-          }
-          console.log(e.point, part);
-
-        }}
-      />
+    <primitive
+      ref={ref}
+      object={object}
+      scale={1}
+      position={position}
+      onClick={(e: any) => {
+        const part = touchBodyPart(e.point, gender);
+        if ((window as any).ReactNativeWebView) {
+          (window as any).ReactNativeWebView?.postMessage(
+            JSON.stringify({ part })
+          );
+        }
+        console.log(e.point, part);
+      }}
+    />
   );
 };
 
-
 function App() {
   const orbitRef = useRef<any>();
-  const [gender, setGender] = useState<Gender>('male');
+  const [gender, setGender] = useState<Gender>("male");
+  const [autoRotation, setAutoRotation] = useState(false);
   useEffect(() => {
     orbitRef?.current?.reset();
-    const qs = new URLSearchParams(window.location.search)
-    if(qs && qs.get('gender')) {
-      setGender(qs.get('gender') as Gender )
+    const qs = new URLSearchParams(window.location.search);
+    if (qs && qs.get("gender")) {
+      setGender(qs.get("gender") as Gender);
     }
+    setAutoRotation(qs.get("autoRotation") === "true");
   }, []);
 
   return (
     <div className="App">
-      <div onClick={() => {
-    orbitRef?.current?.reset();
-
-      }}>Reset</div>
       <Canvas className="canvas3d" gl={{ useLegacyLights: true }}>
         <Suspense fallback={null}>
           <ambientLight castShadow intensity={1} />
           <directionalLight castShadow position={[0, 20, 10]} intensity={2} />
-          <Scene gender={gender} />
+          <Scene gender={gender} autoRotation={autoRotation} />
           <OrbitControls
             ref={orbitRef}
             minPolarAngle={1.5}
